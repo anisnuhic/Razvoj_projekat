a173c5b4422eb323987bc4ba53ffd22b73d6f843
anisnuhic <anis.nuhic30@gmail.com>
2024-08-20 15:02:37 +0200

    par stvari rijeseno

---
+++ eventory/src/main/java/grupa8/Dogadjaj.java
@@ -0,0 +1,16 @@
+package grupa8;
+
+import java.util.Date;
+
+public class Dogadjaj {
+    String nazivDogadjaja;
+    String urlDogadjaja;
+    Date datumDogadjaja;
+
+    public Dogadjaj(String x, String y, Date z){
+        nazivDogadjaja = x;
+        urlDogadjaja = y;
+        datumDogadjaja = z;
+    }
+    
+}
--- eventory/src/main/java/grupa8/PrimaryController.java
+++ eventory/src/main/java/grupa8/PrimaryController.java
@@ -1,13 +1,69 @@
 package grupa8;
 
-import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class PrimaryController {
 
     @FXML
-   AnchorPane root = new AnchorPane();
-    Scene scene = new Scene(root, 1360, 760);
+    private void handlePrijavaButtonAction(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("prijava.fxml"));
+            Parent prijavaRoot = fxmlLoader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Prijava");
+            stage.setScene(new Scene(prijavaRoot));
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(((Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow()));
+            stage.showAndWait();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handleRegistracijaButtonAction(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("registracija.fxml"));
+            Parent registracijaRoot = fxmlLoader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Registracija");
+            stage.setScene(new Scene(registracijaRoot));
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(((Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow()));
+            stage.showAndWait();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+      public static void main(String[] args) {
+        // Kreiraj listu za Dogadjaj objekte
+        List<Dogadjaj> listaDogadjaja = new ArrayList<>();
+        
+        // Kreiraj 5 objekata Dogadjaj sa hardkodiranim vrijednostima
+        listaDogadjaja.add(new Dogadjaj("Koncert u parku", "http://example.com/koncert", new Date())); // 15. avgust 2024
+        listaDogadjaja.add(new Dogadjaj("Izložba umetnosti", "http://example.com/izlozba", new Date())); // 20. septembar 2024
+        listaDogadjaja.add(new Dogadjaj("Sportsko takmičenje", "http://example.com/sport", new Date())); // 10. oktobar 2024
+        listaDogadjaja.add(new Dogadjaj("Pozorišna predstava", "http://example.com/pozoriste", new Date())); // 5. novembar 2024
+        listaDogadjaja.add(new Dogadjaj("Tehnička konferencija", "http://example.com/konferencija", new Date())); // 25. decembar 2024
+
+        // Ispis liste Dogadjaj objekata
+        for (Dogadjaj d : listaDogadjaja) {
+            System.out.println("Naziv: " + d.nazivDogadjaja + ", URL: " + d.urlDogadjaja + ", Datum: " + d.datumDogadjaja);
+        }
+    }
 }
