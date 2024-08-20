f669cdca72463695de8a8cb227912a27e4eeee3e
Anis Nuhic <anis.nuhic30gmail.com>
2024-08-20 06:57:11 +0200

    eventory

---
+++ eventory/src/main/java/grupa8/SecondaryController.java
@@ -0,0 +1,12 @@
+package grupa8;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
