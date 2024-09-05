73f9c73777058ad8a78ba39ac56b1b06ad15a100
Anis Nuhic <anis.nuhic30gmail.com>
2024-09-05 16:20:32 +0200

    main

--- eventory/src/main/java/grupa8/AdminLokacijaController.java
+++ eventory/src/main/java/grupa8/AdminLokacijaController.java
@@ -1,7 +1,14 @@
 package grupa8;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class AdminLokacijaController {
     @FXML
@@ -11,9 +18,24 @@ public class AdminLokacijaController {
     }
     @FXML 
     private void dodajLokaciju(ActionEvent event){
-        
+         try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("dodajLokaciju.fxml"));
+            Parent registracijaRoot = fxmlLoader.load();
+            // DodajLokacijuController adminController = fxmlLoader.getController();
+            // adminController.setPrimaryController(this);
+            Stage stage = new Stage();
+            stage.setTitle("Napravi lokaciju");
+            stage.setScene(new Scene(registracijaRoot));
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(((Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow()));
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    private void initialize(){
+    @FXML
+    public void initialize(){
 
     }
+    
 }
